54879fdfca1cd344d761a7de60d04d0c221a578d
justL <justlikesuolong@outlook.com>
2024-09-27 06:58:21 +0800

    pref: 添加 fastify & 加入欢迎语句

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import { Logger } from '@nestjs/common';
+
+const schema = process.env.SCHEMA || 'http';
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(port);
 }
-bootstrap();
+
+bootstrap().then(() => {
+  const logger = new Logger('      😄       ');
+  logger.log(`Server is running on ${schema}://${host}:${port}`);
+});
